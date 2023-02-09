475645c8a5d5df0a6f7f5fd8d917e063cd92c166
Danil <mydogfromhell666@gmail.com>
2023-02-09 03:09:33 +0300

    add redux localStorage

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const savedTodos = JSON.parse(localStorage.getItem("todos")) ?? [];
+
 const todoSlice = createSlice({
   name: "todos",
   initialState: {
-    todos: [],
+    todos: savedTodos,
   },
   reducers: {
     addTodo(state, action) {
@@ -12,21 +14,26 @@ const todoSlice = createSlice({
         text: action.payload.value,
         isDone: false,
       });
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     removeTodo(state, action) {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     removeAllTodo(state) {
       state.todos = [];
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     removeIsDoneTodo(state) {
       state.todos = state.todos.filter((todo) => !todo.isDone);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     toggleIsDone(state, action) {
       const toggledItem = state.todos.find(
         (todo) => todo.id === action.payload.id
       );
       toggledItem.isDone = !toggledItem.isDone;
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
   },
 });
